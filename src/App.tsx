26160cbe5dce86f13c1ac5927d3ffd77724bac0e
Julia <juliabomfimdeoliveira@hotmail.com>
2021-10-07 19:15:42 -0300

    com add fixo

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { items } from './data/items'
 import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter'
 import { TableArea } from './components/TableArea'
 import { InfoArea } from './components/InfoArea'
+import { InputArea } from './components/InputArea'
 
 const App = () => {
 
@@ -41,6 +42,12 @@ const App = () => {
     setCurrentMonth(newMonth)
   }
 
+  const handleAddItem = (item: Item) => {
+    let newList = [...list];
+    newList.push(item);
+    setList(newList)
+  }
+
   return (
     <C.Container>
       <C.Header>
@@ -58,7 +65,7 @@ const App = () => {
           expense={expense}
         />
         
-        {/*  Area de inserção */}
+        <InputArea onAdd={handleAddItem} />
         
         <TableArea list={filteredList}/>
 
